633415279eed80d81cb40ef5f8146047e334745e
sohil-khann <khaa40408@gmail.com>
2026-01-16 23:18:09 +0530

    [Sohil khan] Add. Program.cs

---
+++ practice_problems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using List;
+using Set;
+using Queue;
+using Map;
+
+public class Program{
+    public static void Main(String[] arg){
+        // Console.WriteLine("--- Set Problems ---");
+        // SetsEqual.Run();
+        // UnionIntersection.Run();
+        // SymmetricDifference.Run();
+        // SetToSortedList.Run();
+        // IsSubset.Run();
+
+        // Console.WriteLine("\n--- Queue Problems ---");
+        // ReverseQueue.Run();
+        // BinaryNumbers.Run();
+        // HospitalTriage.Run();
+
+        // Console.WriteLine("\n--- Map Problems ---");
+        // WordFrequency.Run();
+        InvertMap.Run();
+
+        Console.ReadKey();
+    }
+}
+
